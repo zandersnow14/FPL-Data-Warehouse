ae3b4f9b67eabedefa97245b3bf83c049cc47407
Zandedr Snow <zandersnow144@gmail.com>
2025-11-22 13:23:46 +0000

    Added docstrings

--- main.py
+++ main.py
@@ -8,28 +8,46 @@ BOOTSTRAP_URL = "https://fantasy.premierleague.com/api/bootstrap-static/"
 
 
 def get_gameweek_data() -> pd.DataFrame:
+    """
+    Retrieves metadata about each gameweek from the FPL API.
+
+    :returns: Pandas DataFrame of gameweek metadata.
+    """
 
     res = requests.get(BOOTSTRAP_URL)
     data: dict = res.json()
     gw_df = pd.DataFrame(data['events'])
-    gw_df = gw_df[['id', 'name', 'deadline_time_epoch', 'finished', 'is_current', 'is_previous', 'is_next']]
+    gw_df = gw_df[
+        ['id', 'name', 'deadline_time_epoch', 'finished', 'is_current', 'is_previous', 'is_next']
+        ]
     gw_df['deadline_time'] = gw_df['deadline_time_epoch'].apply(lambda x: datetime.fromtimestamp(x))
     return gw_df
 
 
 def get_player_data() -> pd.DataFrame:
+    """
+    Retrieves raw player data from the FPL API.
+
+    :returns: Pandas DataFrame containing raw player data.
+    """
 
     res = requests.get(BOOTSTRAP_URL)
     data: dict = res.json()
     player_df = pd.DataFrame(data['elements'])
     player_df = player_df[['first_name', 'second_name', 'web_name', 'team', 'now_cost']]
     player_df['now_cost'] = player_df['now_cost'] / 10
-    print(player_df)
+    return player_df
 
 
 def main():
+    """
+    Main function.
+
+    :returns: nothing yet
+    """
     gw_data = get_gameweek_data()
-    
+    print(gw_data)
+
     get_player_data()
 
 
