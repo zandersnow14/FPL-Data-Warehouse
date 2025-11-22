11597d5719b367dc08c4750598498f9fddd63143
Zandedr Snow <zandersnow144@gmail.com>
2025-11-22 12:46:06 +0000

    Added player and gameweek functions

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
+
+from datetime import datetime
+
+import pandas as pd
+import requests
+
+BOOTSTRAP_URL = "https://fantasy.premierleague.com/api/bootstrap-static/"
+
+
+def get_gameweek_data() -> pd.DataFrame:
+
+    res = requests.get(BOOTSTRAP_URL)
+    data: dict = res.json()
+    gw_df = pd.DataFrame(data['events'])
+    gw_df = gw_df[['id', 'name', 'deadline_time_epoch', 'finished', 'is_current', 'is_previous', 'is_next']]
+    gw_df['deadline_time'] = gw_df['deadline_time_epoch'].apply(lambda x: datetime.fromtimestamp(x))
+    return gw_df
+
+
+def get_player_data() -> pd.DataFrame:
+
+    res = requests.get(BOOTSTRAP_URL)
+    data: dict = res.json()
+    player_df = pd.DataFrame(data['elements'])
+    player_df = player_df[['first_name', 'second_name', 'web_name', 'team', 'now_cost']]
+    player_df['now_cost'] = player_df['now_cost'] / 10
+    print(player_df)
+
+
 def main():
-    print("Hello fpl-data-warehouse!")
+    gw_data = get_gameweek_data()
+    
+    get_player_data()
+
 
 
 if __name__ == "__main__":
     main()
+
